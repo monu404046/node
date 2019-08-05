39bdce4dc9c96731f28cda0ade9e9b93adeaab95
ASHWIN RAJ <53168050+monu404046@users.noreply.github.com>
2019-08-05 12:56:45 +0530

    node

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+var x=function(){ 
+console.log("Hello Welcome");
+}
+x();
---
+++ greting.js
@@ -0,0 +1,10 @@
+greeting=require("./app1");
+
+function disp()
+{
+    for(i=0;i<greeting.ar.length;i++)
+    {
+console.log(greeting.ar[i]);
+    }
+}
+disp();
